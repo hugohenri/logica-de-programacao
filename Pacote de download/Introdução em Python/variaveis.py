718ec508dfd1afecd34575d856c07b8e7b55e69f
Hugo Henrique <hugohenrisoares@gmail.com>
2023-03-29 11:59:18 -0300

    Update variaveis.py

--- Pacote de download/Introdução em Python/variaveis.py
+++ Pacote de download/Introdução em Python/variaveis.py
@@ -5,13 +5,13 @@ genero: str
 nome: str
 
 idade = 27
-salario =2000
-altura= 1,75
+salario =2000.00
+altura= 1.75
 genero = "m"
 nome= "Hugo"
 
-print('idade: 'idade)
-print(salario)
-print(altura)
-print(genero)
-print('nome: ' nome)
+print(f"Idade = {idade}")
+print(f"Salário = {salario:.2f}")
+print(f"Altura = {altura:.2f}")
+print(f"Genero = {genero}")
+print(f"Nome = {nome}")
